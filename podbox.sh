03483c7d4e597f6c966beba20b9d83d5c68faa9e
DimaZirix <dmitriy@zirix.com>
2019-09-02 18:58:38 +0000

    parse_container_params

--- podbox.sh
+++ podbox.sh
@@ -37,25 +37,54 @@ function write_settings_file() {
   echo '#volumes' >"$config_file"
   for volume in "${container_volumes[@]}"; do
     echo "$volume" >>"$config_file"
+    echo "vol"
   done
-  echo '#end' >"$config_file"
+  echo '#end' >>"$config_file"
 
   echo '#params' >>"$config_file"
   for param in "${container_params[@]}"; do
     echo "$param" >>"$config_file"
   done
-  echo '#end' >"$config_file"
+  echo '#end' >>"$config_file"
 }
 
-
 function parse_container_params() {
-  echo "$@"
+  while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+      "--X11"|"--x11") isX11=true;;
+      "--audio") isAudio=true;;
+      "--ipc") isIpc=true;;
+      "--user-mapping") isUserMapping=true;;
+      "--network") isNetwork=true;;
+      "--volume")
+        container_volumes+=("$2")
+        shift;;
+      -*)
+        echo "Error: unknown flag: $1"
+        show_ussage_message
+        exit 1;;
+      *)break;;
+    esac
+    shift
+  done
+
+  parse_params=$@
 }
 
 function action_create() {
   local box_name="$1"
   shift
+
   parse_container_params "$@"
+  set -- "$parse_params"
+
+  if [ "$#" -ne "1" ]; then
+    echo "Error: Illegal count of arguments"
+    show_ussage_message
+    exit 1
+  fi
+
+
 
   write_settings_file "$box_name"
 
