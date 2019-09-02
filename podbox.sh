544c18deea5b02ceaaf9268495419194db48f243
DimaZirix <dmitriy@zirix.com>
2019-09-02 18:31:43 +0000

    write_settings_file

--- podbox.sh
+++ podbox.sh
@@ -29,6 +29,24 @@ function read_settings_file() {
   done
 }
 
+function write_settings_file() {
+  local box_name="$1"
+  local config_file="$HOME/.config/podbox/$box_name"
+  mkdir -p "$(dirname "$config_file")"
+
+  echo '#volumes' >"$config_file"
+  for volume in "${container_volumes[@]}"; do
+    echo "$volume" >>"$config_file"
+  done
+  echo '#end' >"$config_file"
+
+  echo '#params' >>"$config_file"
+  for param in "${container_params[@]}"; do
+    echo "$param" >>"$config_file"
+  done
+  echo '#end' >"$config_file"
+}
+
 
 function parse_container_params() {
   echo "$@"
@@ -39,7 +57,7 @@ function action_create() {
   shift
   parse_container_params "$@"
 
-  read_settings_file "$box_name"
+  write_settings_file "$box_name"
 
   echo "action_create $box_name"
 }
